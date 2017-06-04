babfd05d1526a3b1aeef168db48fe845136e0ffd
wilcozhang <wilcozhang@163.com>
2017-06-04 16:48:06 +0800

    Initial commit

---
+++ app/src/main/java/com/wilcozhang/myandroidstudio/MainActivity.java
@@ -0,0 +1,47 @@
+package com.wilcozhang.myandroidstudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Date mDate = new Date();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    private String saySomething() {
+        String result = "SomeThing";
+        return result;
+    }
+
+    private void addName(String name, Bundle bundle) {
+        bundle.putString("Name", name);
+    }
+
+    private Date getDate() {
+        return mDate;
+    }
+
+    private String methodHello() {
+        String greet = "Hello";
+
+        return getGreet(greet);
+    }
+
+    private String getGreet(String gre) {
+        StringBuilder sb = new StringBuilder();
+        for (int nc = 0; nc < 10; ++nc) {
+            sb.append(gre + nc);
+        }
+
+        return sb.toString();
+    }
+}
